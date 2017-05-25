2a43d6046ee1e9a85dc44762676040731d2acf42
msx752 <aslim@outlook.com.tr>
2017-05-25 16:09:46 +0300

    timeouter is configured

--- PoGoEmulator/PoGoEmulator/Models/Connection.cs
+++ PoGoEmulator/PoGoEmulator/Models/Connection.cs
@@ -1,84 +1,64 @@
-﻿using PoGoEmulator.Enums;
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+using PoGoEmulator.Database;
+using PoGoEmulator.Enums;
 using PoGoEmulator.Logging;
 using PoGoEmulator.Requests;
-using System;
-using System.Diagnostics;
-using System.Net.Sockets;
-using System.Reactive;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Timers;
-using PoGoEmulator.Database;
-using PoGoEmulator.Database.Tables;
-using Timer = System.Timers.Timer;
 
 namespace PoGoEmulator.Models
 {
     public sealed class Connection : IDisposable
     {
         private readonly CancellationTokenSource _cts = new CancellationTokenSource();
-        private MyHttpContext _httpContext;
         private TcpClient client;
-        private bool IsDisposed = false;
-        private Stopwatch stopwatch;
-        private NetworkStream _stream;
-        private Timer tmr;
-        private PoGoDbContext _database;
+        private bool _isDisposed;
+        private TimeoutTick _ttick;
 
         public Connection(TcpClient client)
         {
             _cts.Token.ThrowIfCancellationRequested();
             this.client = client;
-            stopwatch = new Stopwatch();
-            tmr = new Timer(150);
-
-            tmr.Elapsed += Tmr_Elapsed;
-            stopwatch.Start();
-            Task.Run(() => tmr.Start(), _cts.Token);
-            this._stream = this.client.GetStream();
-            _httpContext = Stream.GetContext(_cts.Token);
-            _database = new PoGoDbContext();
+            _ttick = new TimeoutTick(_cts.Token, TimeoutChecker, true);
+            Stream = this.client.GetStream();
+            HttpContext = Stream.GetContext(_cts.Token);
+            Database = new PoGoDbContext();
         }
 
-        public PoGoDbContext Database { get { return _database; } }
-        public MyHttpContext HttpContext { get { return _httpContext; } }
-        public NetworkStream Stream { get { return _stream; } }
+        public PoGoDbContext Database { get; }
+        public MyHttpContext HttpContext { get; private set; }
+        public NetworkStream Stream { get; private set; }
 
         public void Abort(RequestState state)
         {
             //You can write out an abort message to the client if you like. (Stream.Write()....)
-            this.Dispose(state);
+            Dispose(state);
         }
 
         private void Dispose(RequestState state)
         {
-            if (IsDisposed || _cts.IsCancellationRequested) return;
+            if (_isDisposed || _cts.IsCancellationRequested) return;
 #if DEBUG
             Logger.Write($"session ended for {client.Client.RemoteEndPoint}, Reason: '{state}'", LogLevel.Debug);
 #endif
-            IsDisposed = true;
+            _isDisposed = true;
 
             if (state == RequestState.Completed)
                 Database.SaveChanges();
 
             Database.Dispose();
-
-            tmr.Enabled = false;
-            tmr.Dispose();
+            _ttick.Stop();
+            _ttick = null;
             _cts.Cancel(); //force stop
-            Thread.Sleep(10);
-            tmr = null;
-
-            stopwatch = null;
-            _httpContext = null;
+            HttpContext = null;
 
             client?.Close();
             ((IDisposable)client)?.Dispose();
             client = null;
 
-            _stream?.Close();
-            _stream?.Dispose();
-            _stream = null;
+            Stream?.Close();
+            Stream?.Dispose();
+            Stream = null;
         }
 
         public void Dispose()
@@ -99,7 +79,7 @@ namespace PoGoEmulator.Models
             {
 #if DEBUG
                 Logger.Write(e.Message, LogLevel.TaskIssue);
-                this.Abort(RequestState.AbortedBySystem);
+                Abort(RequestState.AbortedBySystem);
                 return;
 #endif
             }
@@ -107,28 +87,28 @@ namespace PoGoEmulator.Models
             {
 #if DEBUG
                 Logger.Write(e.Message, LogLevel.TaskIssue);
-                this.Abort(RequestState.AbortedBySystem);
+                Abort(RequestState.AbortedBySystem);
                 return;
 #endif
             }
             catch (Exception e)
             {
                 Logger.Write(e.Message, LogLevel.Error);
-                this.Abort(RequestState.AbortedBySystem);
+                Abort(RequestState.AbortedBySystem);
                 return;
             }
-            this.Abort(RequestState.Completed);
+            Abort(RequestState.Completed);
         }
 
-        private void Tmr_Elapsed(object sender, ElapsedEventArgs e)
+        private void TimeoutChecker()
         {
             if (_cts.Token.IsCancellationRequested)
                 return;
 
-            if (this.client.Client.Poll(1, SelectMode.SelectRead) && this.client.Client.Available == 0)//detect the custom aborting
-                this.Abort(RequestState.CanceledByUser);
-            else if (stopwatch?.ElapsedMilliseconds > Global.Cfg.RequestTimeout.TotalMilliseconds)
-                this.Abort(RequestState.Timeout);
+            if (client.Client.Poll(1, SelectMode.SelectRead) && client.Client.Available == 0)//detect the custom aborting
+                Abort(RequestState.CanceledByUser);
+            else if (_ttick.Stopwatch.ElapsedMilliseconds > Global.Cfg.RequestTimeout.TotalMilliseconds)
+                Abort(RequestState.Timeout);
         }
     }
 }
