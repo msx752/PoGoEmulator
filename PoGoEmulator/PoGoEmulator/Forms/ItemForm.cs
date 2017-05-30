6d96a93dfe3a591b74d0a35760adccabfc547786
--=FurtiF™=-- <msntchat@hotmail.fr>
2017-05-30 02:57:09 +0200

    rebase

---
+++ PoGoEmulator/PoGoEmulator/Forms/ItemForm.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+using POGOProtos.Inventory.Item;
+using PoGoEmulator.Helpers;
+
+namespace PoGoEmulator.Forms
+{
+    public partial class ItemForm : Form
+    {
+        public ItemForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
