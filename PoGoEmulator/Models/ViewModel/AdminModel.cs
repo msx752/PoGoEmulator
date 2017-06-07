532f65d9142a8fb0bcf5d8ab1045c9e138773f38
msx752 <aslim@outlook.com.tr>
2017-06-07 15:21:36 +0300

    viewmodel moved

--- PoGoEmulator/Models/ViewModel/AdminModel.cs
+++ PoGoEmulator/Models/ViewModel/AdminModel.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
-namespace PoGoEmulator.Models
+namespace PoGoEmulator.Models.ViewModel
 {
     public class AdminModel
     {
